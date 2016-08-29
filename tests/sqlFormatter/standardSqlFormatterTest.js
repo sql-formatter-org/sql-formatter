82243ec1f845655614ad644fc9fb5d26f52f91e0
Rene Saarsoo <rene.saarsoo@zeroturnaround.com>
2016-08-29 21:31:32 +0300

    XR-3710 Add more thorough tests SQL variables

--- tests/sqlFormatter/standardSqlFormatterTest.js
+++ tests/sqlFormatter/standardSqlFormatterTest.js
@@ -147,14 +147,29 @@ describe("standardSqlFormatter", function() {
         );
     });
 
-    it("formats SELECT query with different variable types", function() {
+    it("recognizes @variables", function() {
         const result = standardSqlFormatter.format(
-            "SELECT @variable, @'variable name';"
+            "SELECT @variable, @'var name', @\"var name\", @`var name`;"
         );
         expect(result).toBe(
             "SELECT\n" +
             "  @variable,\n" +
-            "  @'variable name';\n"
+            "  @'var name',\n" +
+            "  @\"var name\",\n" +
+            "  @`var name`;\n"
+        );
+    });
+
+    it("recognizes :variables", function() {
+        const result = standardSqlFormatter.format(
+            "SELECT :variable, :'var name', :\"var name\", :`var name`;"
+        );
+        expect(result).toBe(
+            "SELECT\n" +
+            "  :variable,\n" +
+            "  :'var name',\n" +
+            "  :\"var name\",\n" +
+            "  :`var name`;\n"
         );
     });
 
