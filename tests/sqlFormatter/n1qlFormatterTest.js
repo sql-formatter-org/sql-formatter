7fecdd2bfce19aeb90eb512ad931b059cbf75667
Rene Saarsoo <rene.saarsoo@zeroturnaround.com>
2016-08-29 22:15:32 +0300

    XR-3710 Use shared set of tests for bot standard and n1ql formatters
    Most of the syntax is the same, so makes sense to run most of the tests
    against both formatters.

--- tests/sqlFormatter/n1qlFormatterTest.js
+++ tests/sqlFormatter/n1qlFormatterTest.js
@@ -1,40 +1,16 @@
 import n1qlFormatter from "xr/sqlFormatter/n1qlFormatter";
+import behavesLikeSqlFormatter from "tests/specs/sqlFormatter/behavesLikeSqlFormatter";
 
 describe("n1qlFormatter", function() {
-    it("formats SELECT query with CLOCK_STR function", function() {
-        const result = n1qlFormatter.format("SELECT CLOCK_STR(*),'Column1' FROM Table1;");
-        expect(result).toBe(
-            "SELECT\n" +
-            "  CLOCK_STR(*),\n" +
-            "  'Column1'\n" +
-            "FROM\n" +
-            "  Table1;\n"
-        );
-    });
+    behavesLikeSqlFormatter(n1qlFormatter);
 
-    it("formats SELECT query with DISTINCT keyword", function() {
-        const result = n1qlFormatter.format("SELECT DISTINCT orderlines[0].productId FROM orders;");
+    it("formats SELECT query with element selection expression", function() {
+        const result = n1qlFormatter.format("SELECT orderlines[0].productId FROM orders;");
         expect(result).toBe(
             "SELECT\n" +
-            "  DISTINCT orderlines[0].productId\n" +
+            "  orderlines[0].productId\n" +
             "FROM\n" +
             "  orders;\n"
-        );
-    });
-
-    it("formats SELECT query with data as function arg", function() {
-        const result = n1qlFormatter.format(
-            "SELECT fname, age, ROUND(age/7) AS age_dog_years FROM tutorial WHERE fname='Dave';"
-        );
-        expect(result).toBe(
-            "SELECT\n" +
-            "  fname,\n" +
-            "  age,\n" +
-            "  ROUND(age / 7) AS age_dog_years\n" +
-            "FROM\n" +
-            "  tutorial\n" +
-            "WHERE\n" +
-            "  fname = 'Dave';\n"
         );
     });
 
@@ -53,46 +29,31 @@ describe("n1qlFormatter", function() {
         );
     });
 
-    it("formats SELECT query with UNNEST and other toplevel words", function() {
+    it("formats SELECT query with UNNEST toplevel reserver word", function() {
         const result = n1qlFormatter.format(
-            "SELECT t.relation, COUNT(*) AS count, AVG(c.age) AS avg_age FROM tutorial t " +
-            "UNNEST t.children c WHERE c.age > 10 GROUP BY t.relation HAVING COUNT(*) > 1 " +
-            "ORDER BY avg_age DESC LIMIT 1 OFFSET 1;"
+            "SELECT * FROM tutorial UNNEST tutorial.children c;"
         );
         expect(result).toBe(
             "SELECT\n" +
-            "  t.relation,\n" +
-            "  COUNT(*) AS count,\n" +
-            "  AVG(c.age) AS avg_age\n" +
+            "  *\n" +
             "FROM\n" +
-            "  tutorial t\n" +
+            "  tutorial\n" +
             "UNNEST\n" +
-            "  t.children c\n" +
-            "WHERE\n" +
-            "  c.age > 10\n" +
-            "GROUP BY\n" +
-            "  t.relation\n" +
-            "HAVING\n" +
-            "  COUNT(*) > 1\n" +
-            "ORDER BY\n" +
-            "  avg_age DESC\n" +
-            "LIMIT\n" +
-            "  1 OFFSET 1;\n"
+            "  tutorial.children c;\n"
         );
     });
 
-    it("formats SELECT query with NEST and other same-line reserved words", function() {
+    it("formats SELECT query with NEST and USE KEYS", function() {
         const result = n1qlFormatter.format(
-            "SELECT usr.personal_details, orders FROM users_with_orders usr " +
+            "SELECT * FROM usr " +
             "USE KEYS 'Elinor_33313792' NEST orders_with_users orders " +
             "ON KEYS ARRAY s.order_id FOR s IN usr.shipped_order_history END;"
         );
         expect(result).toBe(
             "SELECT\n" +
-            "  usr.personal_details,\n" +
-            "  orders\n" +
+            "  *\n" +
             "FROM\n" +
-            "  users_with_orders usr\n" +
+            "  usr\n" +
             "USE KEYS\n" +
             "  'Elinor_33313792'\n" +
             "NEST\n" +
@@ -100,7 +61,7 @@ describe("n1qlFormatter", function() {
         );
     });
 
-    it("formats explained DELETE query", function() {
+    it("formats explained DELETE query with USE KEYS and RETURNING", function() {
         const result = n1qlFormatter.format(
             "EXPLAIN DELETE FROM tutorial t USE KEYS 'baldwin' RETURNING t"
         );
@@ -112,7 +73,7 @@ describe("n1qlFormatter", function() {
         );
     });
 
-    it("formats UPDATE query", function() {
+    it("formats UPDATE query with USE KEYS and RETURNING", function() {
         const result = n1qlFormatter.format(
             "UPDATE tutorial USE KEYS 'baldwin' SET type = 'actor' RETURNING tutorial.type"
         );
