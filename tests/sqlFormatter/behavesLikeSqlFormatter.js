6fb7edaa211fa5f74fe1989c1926ca089119b7af
Rene Saarsoo <rene.saarsoo@zeroturnaround.com>
2016-08-30 09:32:03 +0300

    XR-3710 Move parenthesis test to generic SQL formatter test

--- tests/sqlFormatter/behavesLikeSqlFormatter.js
+++ tests/sqlFormatter/behavesLikeSqlFormatter.js
@@ -188,6 +188,34 @@ export default function behavesLikeSqlFormatter(formatter) {
         );
     });
 
+    it("breaks long parenthized lists to multiple lines", function() {
+        const result = formatter.format(
+            "INSERT INTO some_table (id_product, id_shop, id_currency, id_country, id_group, priority, " +
+            "price, from_quantity, reduction,   reduction_type, person, name) (" +
+            "SELECT IF(dq.id_discounter_shopping = 2, dq.value, dq.value / 100)," +
+            "IF (dq.id_discounter_shopping = 2, 'amount', 'percentage') FROM foo);"
+        );
+        expect(result).toBe(
+            "INSERT INTO some_table (\n" +
+            "  id_product, id_shop, id_currency, id_country,\n" +
+            "  id_group, priority, price, from_quantity,\n" +
+            "  reduction, reduction_type, person, name\n" +
+            ") (\n" +
+            "  SELECT\n" +
+            "    IF(\n" +
+            "      dq.id_discounter_shopping = 2, dq.value,\n" +
+            "      dq.value / 100\n" +
+            "    ),\n" +
+            "    IF (\n" +
+            "      dq.id_discounter_shopping = 2, 'amount',\n" +
+            "      'percentage'\n" +
+            "    )\n" +
+            "  FROM\n" +
+            "    foo\n" +
+            ");\n"
+        );
+    });
+
     it("formats simple UPDATE query", function() {
         const result = formatter.format(
             "UPDATE Customers SET ContactName='Alfred Schmidt', City='Hamburg' WHERE CustomerName='Alfreds Futterkiste';"
--- tests/sqlFormatter/standardSqlFormatterTest.js
+++ tests/sqlFormatter/standardSqlFormatterTest.js
@@ -3,32 +3,4 @@ import behavesLikeSqlFormatter from "tests/specs/sqlFormatter/behavesLikeSqlForm
 
 describe("standardSqlFormatter", function() {
     behavesLikeSqlFormatter(standardSqlFormatter);
-
-    it("breaks long parenthized lists to multiple lines", function() {
-        const result = standardSqlFormatter.format(
-            "INSERT INTO some_table (id_product, id_shop, id_currency, id_country, id_group, priority, " +
-            "price, from_quantity, reduction,   reduction_type, person, name) (" +
-            "SELECT IF(dq.id_discounter_shopping = 2, dq.value, dq.value / 100)," +
-            "IF (dq.id_discounter_shopping = 2, 'amount', 'percentage') FROM foo);"
-        );
-        expect(result).toBe(
-            "INSERT INTO some_table (\n" +
-            "  id_product, id_shop, id_currency, id_country,\n" +
-            "  id_group, priority, price, from_quantity,\n" +
-            "  reduction, reduction_type, person, name\n" +
-            ") (\n" +
-            "  SELECT\n" +
-            "    IF(\n" +
-            "      dq.id_discounter_shopping = 2, dq.value,\n" +
-            "      dq.value / 100\n" +
-            "    ),\n" +
-            "    IF (\n" +
-            "      dq.id_discounter_shopping = 2, 'amount',\n" +
-            "      'percentage'\n" +
-            "    )\n" +
-            "  FROM\n" +
-            "    foo\n" +
-            ");\n"
-        );
-    });
 });
