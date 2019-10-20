0c8bf10609b2db333098ea8d0fdc0e7677f6d945
Adrien Pyke <adpyke@gmail.com>
2019-10-20 15:27:02 -0400

    add unicode test case

--- test/behavesLikeSqlFormatter.js
+++ test/behavesLikeSqlFormatter.js
@@ -409,4 +409,9 @@ export default function behavesLikeSqlFormatter(language) {
                 '  Table2;'
         );
     });
+
+    it('formats unicode correctly', function() {
+        const result = format('SELECT test, тест FROM table;');
+        expect(result).toBe('SELECT\n  test,\n  тест\nFROM\n  table;');
+    });
 }
