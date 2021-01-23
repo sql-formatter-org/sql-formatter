930c1164c4793dd1457148a65705c8553e0bddf6
Rene Saarsoo <nene@triin.net>
2021-01-23 16:55:07 +0200

    Change Bool to Boolean in type annotations

--- src/sqlFormatter.js
+++ src/sqlFormatter.js
@@ -22,7 +22,7 @@ export const FORMATTERS = {
  * @param {Object} cfg
  *  @param {String} cfg.language Query language, default is Standard SQL
  *  @param {String} cfg.indent Characters used for indentation, default is "  " (2 spaces)
- *  @param {Bool} cfg.uppercase Converts keywords to uppercase
+ *  @param {Boolean} cfg.uppercase Converts keywords to uppercase
  *  @param {Integer} cfg.linesBetweenQueries How many line breaks between queries
  *  @param {Object} cfg.params Collection of params for placeholder replacement
  * @return {String}
