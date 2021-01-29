cbfde83027cfd0bc157517bc8da02324236a7fef
Rene Saarsoo <nene@triin.net>
2021-01-29 14:45:46 +0200

    Cleanup standard SQL keywords list

--- test/StandardSqlFormatterTest.js
+++ test/StandardSqlFormatterTest.js
@@ -4,7 +4,6 @@ import behavesLikeSqlFormatter from './behavesLikeSqlFormatter';
 import supportsCase from './features/case';
 import supportsCreateTable from './features/createTable';
 import supportsAlterTable from './features/alterTable';
-import supportsAlterTableModify from './features/alterTableModify';
 import supportsSchema from './features/schema';
 import supportsStrings from './features/strings';
 import supportsBetween from './features/between';
@@ -17,23 +16,10 @@ describe('StandardSqlFormatter', () => {
   supportsCase(format);
   supportsCreateTable(format);
   supportsAlterTable(format);
-  supportsAlterTableModify(format);
   supportsStrings(format, ['""', "''"]);
   supportsBetween(format);
   supportsSchema(format);
   supportsJoin(format);
-
-  it('formats INSERT without INTO', () => {
-    const result = sqlFormatter.format(
-      "INSERT Customers (ID, MoneyBalance, Address, City) VALUES (12,-123.4, 'Skagen 2111','Stv');"
-    );
-    expect(result).toBe(dedent`
-      INSERT
-        Customers (ID, MoneyBalance, Address, City)
-      VALUES
-        (12, -123.4, 'Skagen 2111', 'Stv');
-    `);
-  });
 
   it('replaces ? indexed placeholders with param values', () => {
     const result = format('SELECT ?, ?, ?;', {
@@ -44,19 +30,6 @@ describe('StandardSqlFormatter', () => {
         first,
         second,
         third;
-    `);
-  });
-
-  it('formats query with GO batch separator', () => {
-    const result = format('SELECT 1 GO SELECT 2', {
-      params: ['first', 'second', 'third'],
-    });
-    expect(result).toBe(dedent`
-      SELECT
-        1
-      GO
-      SELECT
-        2
     `);
   });
 
--- test/features/schema.js
+++ test/features/schema.js
@@ -6,12 +6,10 @@ import dedent from 'dedent-js';
  */
 export default function supportsSchema(format) {
   it('formats simple SET SCHEMA statements', () => {
-    const result = format('SET SCHEMA schema1; SET CURRENT SCHEMA schema2;');
+    const result = format('SET SCHEMA schema1;');
     expect(result).toBe(dedent`
       SET SCHEMA
         schema1;
-      SET CURRENT SCHEMA
-        schema2;
     `);
   });
 }
