d091d6e9cc1de86b3542c8c56d9ab1dedc79142b
inferrinizzard <inferrinizzard@gmail.com>
2021-09-25 22:50:47 -0700

    add unit test for semicolon newline

--- test/features/operators.js
+++ test/features/operators.js
@@ -1,3 +1,5 @@
+import dedent from 'dedent-js';
+
 /**
  * Tests support for various operators
  * @param {Function} format
@@ -8,5 +10,26 @@ export default function supportsOperators(format, operators = []) {
 		it(`supports ${op} operator`, () => {
 			expect(format(`foo${op}bar`)).toBe(`foo ${op} bar`);
 		});
+	});
+
+	it('supports semicolon on same line', () => {
+		const result = format(`SELECT a FROM b;`);
+		expect(result).toBe(dedent`
+      SELECT
+        a
+      FROM
+        b;
+		`);
+	});
+
+	it('supports semicolon on new line', () => {
+		const result = format(`SELECT a FROM b;`, { semicolonNewline: true });
+		expect(result).toBe(dedent`
+      SELECT
+        a
+      FROM
+        b
+      ;
+		`);
 	});
 }
