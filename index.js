78cc370cc2b7fd838f3c293520952d22aec77542
inferrinizzard <inferrinizzard@gmail.com>
2021-11-22 07:43:18 -0800

    add new options to demo

--- index.js
+++ index.js
@@ -3,21 +3,73 @@ const attachFormat = () => {
 	const uppercase = document.getElementById('uppercase');
 	const input = document.getElementById('input');
 	const output = document.getElementById('output');
+	const keywordPosition = document.getElementById('keywordPosition');
+	const breakBeforeBooleanOperator = document.getElementById('breakBeforeBooleanOperator');
+	const aliasAs = document.getElementById('aliasAs');
+	const newline = document.getElementById('newline');
+	const itemCount = document.getElementById('newline.itemCount');
+	const tabulateAlias = document.getElementById('tabulateAlias');
+	const commaPosition = document.getElementById('commaPosition');
+	const openParenNewline = document.getElementById('openParenNewline');
+	const closeParenNewline = document.getElementById('closeParenNewline');
+	const lineWidth = document.getElementById('lineWidth');
+	const lineBetweenQueries = document.getElementById('lineBetweenQueries');
+	const denseOperators = document.getElementById('denseOperators');
+	const semicolonNewline = document.getElementById('semicolonNewline');
 
 	function format() {
 		console.time('formatting');
-		output.value = prettierSql.format(input.value, {
+		const config = {
 			language: language.options[language.selectedIndex].value,
 			uppercase: uppercase.checked,
-		});
+			keywordPosition: keywordPosition.options[keywordPosition.selectedIndex].value,
+			breakBeforeBooleanOperator: breakBeforeBooleanOperator.checked,
+			aliasAs: aliasAs.checked,
+			newline: {
+				mode: newline.options[newline.selectedIndex].value,
+				itemCount: itemCount.value,
+			},
+			tabulateAlias: tabulateAlias.checked,
+			commaPosition: commaPosition.options[commaPosition.selectedIndex].value,
+			parenOptions: {
+				openParenNewline: openParenNewline.checked,
+				closeParenNewline: closeParenNewline.checked,
+			},
+			lineWidth: lineWidth.value,
+			lineBetweenQueries: lineBetweenQueries.value,
+			denseOperators: denseOperators.checked,
+			semicolonNewline: semicolonNewline.checked,
+		};
+		output.value = prettierSql.format(input.value, config);
 		console.timeEnd('formatting');
 	}
 
 	input.addEventListener('input', format);
-	language.addEventListener('change', format);
-	uppercase.addEventListener('change', format);
+	[
+		language,
+		uppercase,
+		keywordPosition,
+		newline,
+		itemCount,
+		breakBeforeBooleanOperator,
+		aliasAs,
+		tabulateAlias,
+		commaPosition,
+		openParenNewline,
+		closeParenNewline,
+		lineWidth,
+		lineBetweenQueries,
+		denseOperators,
+		semicolonNewline,
+	].forEach(option => option.addEventListener('change', format));
 
 	format();
+
+	const fontSize = document.getElementById('font-size');
+	fontSize.addEventListener('change', () => {
+		input.style.fontSize = fontSize.value + 'px';
+		output.style.fontSize = fontSize.value + 'px';
+	});
 };
 
 document.addEventListener('DOMContentLoaded', attachFormat);
