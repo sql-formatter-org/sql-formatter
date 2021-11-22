2e32826990520444e1f10719f6030f4bdb6d9307
inferrinizzard <inferrinizzard@gmail.com>
2021-11-21 16:56:44 -0800

    split into css and js files

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const language = document.getElementById('language');
+	const uppercase = document.getElementById('uppercase');
+	const input = document.getElementById('input');
+	const output = document.getElementById('output');
+
+	function format() {
+		console.time('formatting');
+		output.value = prettierSql.format(input.value, {
+			language: language.options[language.selectedIndex].value,
+			uppercase: uppercase.checked,
+		});
+		console.timeEnd('formatting');
+	}
+
+	input.addEventListener('input', format);
+	language.addEventListener('change', format);
+	uppercase.addEventListener('change', format);
+
+	format();
+});
