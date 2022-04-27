cee2c8f0f1c00c60f87f8e1dd0661914e162b254
Rene Saarsoo <nene@triin.net>
2022-04-27 18:00:21 +0300

    Switch from TABs back to SPACEs

--- index.js
+++ index.js
@@ -1,80 +1,80 @@
 const attachFormat = () => {
-	const language = document.getElementById('language');
-	const uppercase = document.getElementById('uppercase');
-	const input = document.getElementById('input');
-	const output = document.getElementById('output');
-	const keywordPosition = document.getElementById('keywordPosition');
-	const breakBeforeBooleanOperator = document.getElementById('breakBeforeBooleanOperator');
-	const aliasAs = document.getElementById('aliasAs');
-	const newline = document.getElementById('newline');
-	const itemCount = document.getElementById('newline.itemCount');
-	const tabulateAlias = document.getElementById('tabulateAlias');
-	const commaPosition = document.getElementById('commaPosition');
-	const openParenNewline = document.getElementById('openParenNewline');
-	const closeParenNewline = document.getElementById('closeParenNewline');
-	const lineWidth = document.getElementById('lineWidth');
-	const lineBetweenQueries = document.getElementById('lineBetweenQueries');
-	const denseOperators = document.getElementById('denseOperators');
-	const semicolonNewline = document.getElementById('semicolonNewline');
+  const language = document.getElementById('language');
+  const uppercase = document.getElementById('uppercase');
+  const input = document.getElementById('input');
+  const output = document.getElementById('output');
+  const keywordPosition = document.getElementById('keywordPosition');
+  const breakBeforeBooleanOperator = document.getElementById('breakBeforeBooleanOperator');
+  const aliasAs = document.getElementById('aliasAs');
+  const newline = document.getElementById('newline');
+  const itemCount = document.getElementById('newline.itemCount');
+  const tabulateAlias = document.getElementById('tabulateAlias');
+  const commaPosition = document.getElementById('commaPosition');
+  const openParenNewline = document.getElementById('openParenNewline');
+  const closeParenNewline = document.getElementById('closeParenNewline');
+  const lineWidth = document.getElementById('lineWidth');
+  const lineBetweenQueries = document.getElementById('lineBetweenQueries');
+  const denseOperators = document.getElementById('denseOperators');
+  const semicolonNewline = document.getElementById('semicolonNewline');
 
-	function format() {
-		try {
-			const config = {
-				language: language.options[language.selectedIndex].value,
-				uppercase: uppercase.checked,
-				keywordPosition: keywordPosition.options[keywordPosition.selectedIndex].value,
-				breakBeforeBooleanOperator: breakBeforeBooleanOperator.checked,
-				aliasAs: aliasAs.options[aliasAs.selectedIndex].value,
-				newline:
-					itemCount.value > 0 ? itemCount.value : newline.options[newline.selectedIndex].value,
-				tabulateAlias: tabulateAlias.checked,
-				commaPosition: commaPosition.options[commaPosition.selectedIndex].value,
-				parenOptions: {
-					openParenNewline: openParenNewline.checked,
-					closeParenNewline: closeParenNewline.checked,
-				},
-				lineWidth: lineWidth.value,
-				lineBetweenQueries: lineBetweenQueries.value,
-				denseOperators: denseOperators.checked,
-				semicolonNewline: semicolonNewline.checked,
-			};
-			output.value = sqlFormatter.format(input.value, config);
-		} catch (e) {
-			output.value = `
+  function format() {
+    try {
+      const config = {
+        language: language.options[language.selectedIndex].value,
+        uppercase: uppercase.checked,
+        keywordPosition: keywordPosition.options[keywordPosition.selectedIndex].value,
+        breakBeforeBooleanOperator: breakBeforeBooleanOperator.checked,
+        aliasAs: aliasAs.options[aliasAs.selectedIndex].value,
+        newline:
+          itemCount.value > 0 ? itemCount.value : newline.options[newline.selectedIndex].value,
+        tabulateAlias: tabulateAlias.checked,
+        commaPosition: commaPosition.options[commaPosition.selectedIndex].value,
+        parenOptions: {
+          openParenNewline: openParenNewline.checked,
+          closeParenNewline: closeParenNewline.checked,
+        },
+        lineWidth: lineWidth.value,
+        lineBetweenQueries: lineBetweenQueries.value,
+        denseOperators: denseOperators.checked,
+        semicolonNewline: semicolonNewline.checked,
+      };
+      output.value = sqlFormatter.format(input.value, config);
+    } catch (e) {
+      output.value = `
 An Error Occurred, please report this at:
 https://github.com/zeroturnaround/sql-formatter/issues\n
 Stack Trace:
 ${e.stack.toString()}
 `;
-		}
-	}
+    }
+  }
 
-	input.addEventListener('input', format);
-	[
-		language,
-		uppercase,
-		keywordPosition,
-		newline,
-		itemCount,
-		breakBeforeBooleanOperator,
-		aliasAs,
-		tabulateAlias,
-		commaPosition,
-		openParenNewline,
-		closeParenNewline,
-		lineWidth,
-		lineBetweenQueries,
-		denseOperators,
-		semicolonNewline,
-	].forEach(option => option.addEventListener('change', format));
+  input.addEventListener('input', format);
+  [
+    language,
+    uppercase,
+    keywordPosition,
+    newline,
+    itemCount,
+    breakBeforeBooleanOperator,
+    aliasAs,
+    tabulateAlias,
+    commaPosition,
+    openParenNewline,
+    closeParenNewline,
+    lineWidth,
+    lineBetweenQueries,
+    denseOperators,
+    semicolonNewline,
+  ].forEach(option => option.addEventListener('change', format));
 
-	format();
+  format();
 
-	const fontSize = document.getElementById('font-size');
-	fontSize.addEventListener('change', () => {
-		input.style.fontSize = fontSize.value + 'px';
-		output.style.fontSize = fontSize.value + 'px';
-	});
+  const fontSize = document.getElementById('font-size');
+  fontSize.addEventListener('change', () => {
+    input.style.fontSize = fontSize.value + 'px';
+    output.style.fontSize = fontSize.value + 'px';
+  });
 };
 
 document.addEventListener('DOMContentLoaded', attachFormat);
