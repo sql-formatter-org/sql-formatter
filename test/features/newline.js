653629c45cee9c2c45313a035ac857cf86f878ee
Rene Saarsoo <nene@triin.net>
2022-04-29 18:54:32 +0300

    More elaborate tests for newline: number

--- test/features/newline.js
+++ test/features/newline.js
@@ -57,18 +57,95 @@ export default function supportsNewlineOptions(language, format) {
     `);
   });
 
-  it('supports itemCount mode', () => {
-    const result = format('SELECT foo, bar, baz, qux FROM corge;', {
-      newline: 3,
+  describe('newline: number', () => {
+    it('splits to multiple lines when more clauses than than the specified number', () => {
+      const result = format('SELECT foo, bar, baz, qux FROM corge;', {
+        newline: 3,
+      });
+      expect(result).toBe(dedent`
+        SELECT
+          foo,
+          bar,
+          baz,
+          qux
+        FROM corge;
+      `);
     });
-    expect(result).toBe(dedent`
-      SELECT
-        foo,
-        bar,
-        baz,
-        qux
-      FROM corge;
-    `);
+
+    it('does not split to multiple lines when the same number of clauses as specified number', () => {
+      const result = format('SELECT foo, bar, baz FROM corge;', {
+        newline: 3,
+      });
+      expect(result).toBe(dedent`
+        SELECT foo, bar, baz
+        FROM corge;
+      `);
+    });
+
+    it('does not split to multiple lines when less clauses than than the specified number', () => {
+      const result = format('SELECT foo, bar FROM corge;', {
+        newline: 3,
+      });
+      expect(result).toBe(dedent`
+        SELECT foo, bar
+        FROM corge;
+      `);
+    });
+
+    it('regardless of count, splits up long clauses (exceeding default lineWidth 50)', () => {
+      const result = format(
+        'SELECT customers.phone_number AS phone, customers.address AS addr FROM customers;',
+        {
+          newline: 3,
+        }
+      );
+      expect(result).toBe(dedent`
+        SELECT
+          customers.phone_number AS phone,
+          customers.address AS addr
+        FROM customers;
+      `);
+    });
+
+    it('does not smaller nr of clauses when their line width is exactly 50', () => {
+      const result = format('SELECT customer.phone phone, customer.addr AS addr FROM customers;', {
+        newline: 3,
+      });
+      expect(result).toBe(dedent`
+        SELECT customer.phone phone, customer.addr AS addr
+        FROM customers;
+      `);
+    });
+
+    it('splits up even short clauses when lineWidth is small', () => {
+      const result = format('SELECT foo, bar FROM customers GROUP BY foo, bar;', {
+        newline: 3,
+        lineWidth: 10,
+      });
+      expect(result).toBe(dedent`
+        SELECT
+          foo,
+          bar
+        FROM
+          customers
+        GROUP BY
+          foo,
+          bar;
+      `);
+    });
+
+    it('ignores commas inside parenthesis when counting clauses', () => {
+      const result = format('SELECT foo, (SELECT a, b, c FROM table2) AS bar FROM table1;', {
+        newline: 3,
+      });
+      expect(result).toBe(dedent`
+        SELECT foo, (
+            SELECT a, b, c
+            FROM table2
+          ) AS bar
+        FROM table1;
+      `);
+    });
   });
 
   it('supports lineWidth mode', () => {
@@ -83,24 +160,6 @@ export default function supportsNewlineOptions(language, format) {
         baz,
         qux
       FROM corge;
-    `);
-  });
-
-  it('supports hybrid mode', () => {
-    const result = format('SELECT verylongfoo, verylongbar FROM baz GROUP BY foo, bar, baz, qux;', {
-      newline: 2,
-      lineWidth: 30,
-    });
-    expect(result).toBe(dedent`
-      SELECT
-        verylongfoo,
-        verylongbar
-      FROM baz
-      GROUP BY
-        foo,
-        bar,
-        baz,
-        qux;
     `);
   });
 }
