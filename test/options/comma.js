641b829ce5377422284045664b1bc028273911b2
Rene Saarsoo <nene@triin.net>
2022-04-29 21:56:02 +0300

    More elaborate tests for commaPosition

--- test/options/comma.js
+++ test/options/comma.js
@@ -4,7 +4,7 @@ import { CommaPosition } from '../../src/types';
 export default function supportsCommaModes(language, format) {
   it('defaults to comma after column', () => {
     const result = format(
-      'SELECT alpha, MAX(beta), delta AS d, epsilon FROM gamma GROUP BY alpha, delta, epsilon'
+      'SELECT alpha , MAX(beta) , delta AS d ,epsilon FROM gamma GROUP BY alpha , delta, epsilon'
     );
     expect(result).toBe(
       dedent(`
@@ -23,47 +23,134 @@ export default function supportsCommaModes(language, format) {
     );
   });
 
-  it('supports comma before column', () => {
-    const result = format(
-      'SELECT alpha, MAX(beta), delta AS d, epsilon FROM gamma GROUP BY alpha, delta, epsilon',
-      { commaPosition: CommaPosition.before }
-    );
-    expect(result).toBe(
-      dedent(`
-        SELECT
-          alpha
-        , MAX(beta)
-        , delta AS d
-        , epsilon
-        FROM
-          gamma
-        GROUP BY
-          alpha
-        , delta
-        , epsilon
-      `)
-    );
+  describe('commaPosition: before', () => {
+    it('adds comma before column', () => {
+      const result = format(
+        'SELECT alpha, MAX(beta), delta AS d, epsilon FROM gamma GROUP BY alpha, delta, epsilon',
+        { commaPosition: CommaPosition.before }
+      );
+      expect(result).toBe(
+        dedent(`
+          SELECT
+            alpha
+          , MAX(beta)
+          , delta AS d
+          , epsilon
+          FROM
+            gamma
+          GROUP BY
+            alpha
+          , delta
+          , epsilon
+        `)
+      );
+    });
+
+    it('works with larger indent', () => {
+      const result = format(
+        'SELECT alpha, MAX(beta), delta AS d, epsilon FROM gamma GROUP BY alpha, delta, epsilon',
+        { commaPosition: CommaPosition.before, indent: '    ' }
+      );
+      expect(result).toBe(
+        dedent(`
+          SELECT
+              alpha
+            , MAX(beta)
+            , delta AS d
+            , epsilon
+          FROM
+              gamma
+          GROUP BY
+              alpha
+            , delta
+            , epsilon
+        `)
+      );
+    });
+
+    // It seems that this style is fundamentally incompatible with tabs
+    it('does not work with tabs :(', () => {
+      const result = format(
+        'SELECT alpha, MAX(beta), delta AS d, epsilon FROM gamma GROUP BY alpha, delta, epsilon',
+        { commaPosition: CommaPosition.before, indent: '\t' }
+      );
+      expect(result).toBe(
+        [
+          'SELECT',
+          '\talpha',
+          '  , MAX(beta)',
+          '  , delta AS d',
+          '  , epsilon',
+          'FROM',
+          '\tgamma',
+          'GROUP BY',
+          '\talpha',
+          '  , delta',
+          '  , epsilon',
+        ].join('\n')
+      );
+    });
   });
 
-  it('supports tabular mode', () => {
-    const result = format(
-      'SELECT alpha, MAX(beta), delta AS d, epsilon FROM gamma GROUP BY alpha, delta, epsilon',
-      { commaPosition: CommaPosition.tabular }
-    );
-    expect(result).toBe(
-      dedent(`
-        SELECT
-          alpha     ,
-          MAX(beta) ,
-          delta AS d,
-          epsilon
-        FROM
-          gamma
-        GROUP BY
-          alpha   ,
-          delta   ,
-          epsilon
-      `)
-    );
+  describe('commaPosition: tabular', () => {
+    it('aligns commas to a column', () => {
+      const result = format(
+        'SELECT alpha, MAX(beta), delta AS d, epsilon FROM gamma GROUP BY alpha, delta, epsilon',
+        { commaPosition: CommaPosition.tabular }
+      );
+      expect(result).toBe(
+        dedent(`
+          SELECT
+            alpha     ,
+            MAX(beta) ,
+            delta AS d,
+            epsilon
+          FROM
+            gamma
+          GROUP BY
+            alpha   ,
+            delta   ,
+            epsilon
+        `)
+      );
+    });
+
+    it('is not effected by indent size', () => {
+      const result = format(
+        'SELECT alpha, MAX(beta), delta AS d, epsilon FROM gamma GROUP BY alpha, delta, epsilon',
+        { commaPosition: CommaPosition.tabular, indent: '      ' }
+      );
+      expect(result).toBe(
+        dedent(`
+          SELECT
+                alpha     ,
+                MAX(beta) ,
+                delta AS d,
+                epsilon
+          FROM
+                gamma
+          GROUP BY
+                alpha   ,
+                delta   ,
+                epsilon
+        `)
+      );
+    });
+
+    it('handles tabs', () => {
+      const result = format('SELECT alpha, MAX(beta), delta AS d, epsilon', {
+        commaPosition: CommaPosition.tabular,
+        indent: '\t',
+      });
+      expect(result).toBe(
+        dedent(`
+          SELECT
+          \talpha     ,
+          \tMAX(beta) ,
+          \tdelta AS d,
+          \tepsilon
+        `)
+      );
+    });
   });
 }
