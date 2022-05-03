bbcfb0e2de2757493535cc9511e92c4c4a901dbd
Rene Saarsoo <nene@triin.net>
2022-05-03 11:33:33 +0300

    Additional test for newlineBeforeParen

--- test/options/newlineBeforeParen.js
+++ test/options/newlineBeforeParen.js
@@ -1,4 +1,6 @@
+import { expect } from '@jest/globals';
 import dedent from 'dedent-js';
+import { KeywordMode } from '../../src/types';
 
 export default function supportsNewlineBeforeParen(language, format) {
   it('defaults to newline before opening and closing parenthesis', () => {
@@ -61,6 +63,29 @@ export default function supportsNewlineBeforeParen(language, format) {
           b
         FROM
           c );
+    `);
+  });
+
+  // TODO: I think this is not as intended.
+  it('has no effect when used together with keywordPosition:tenSpaceLeft', () => {
+    const withNewlineOn = format('SELECT a FROM ( SELECT b FROM c );', {
+      newlineBeforeOpenParen: true,
+      newlineBeforeCloseParen: true,
+      keywordPosition: KeywordMode.tenSpaceLeft,
+    });
+    const withNewlineOff = format('SELECT a FROM ( SELECT b FROM c );', {
+      newlineBeforeOpenParen: false,
+      newlineBeforeCloseParen: false,
+      keywordPosition: KeywordMode.tenSpaceLeft,
+    });
+
+    expect(withNewlineOn).toBe(withNewlineOff);
+    expect(withNewlineOn).toBe(dedent`
+    SELECT    a
+    FROM      (
+              SELECT    b
+              FROM      c
+              );
     `);
   });
 }
