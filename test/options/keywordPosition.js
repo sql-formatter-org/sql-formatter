d8f0b34910cfa1e2895631591b4a6ee61610698e
Rene Saarsoo <nene@triin.net>
2022-05-03 11:15:44 +0300

    Inline single-use variables in keywordPosition tests

--- test/options/keywordPosition.js
+++ test/options/keywordPosition.js
@@ -10,73 +10,69 @@ export default function supportsKeywordPositions(language, format) {
     GROUP BY column4;
   `;
 
-  const standardResult = dedent(`
-    SELECT
-      COUNT(a.column1),
-      MAX(b.column2 + b.column3),
-      b.column4 AS four
-    FROM
-    (
-      SELECT
-        column1,
-        column5
-      FROM
-        table1
-    ) a
-    JOIN table2 b
-    ON a.column5 = b.column5
-    WHERE
-      column6
-      AND column7
-    GROUP BY
-      column4;
-    `);
-
-  const tenSpaceLeftResult = dedent(`
-    SELECT    COUNT(a.column1),
-              MAX(b.column2 + b.column3),
-              b.column4 AS four
-    FROM      (
-              SELECT    column1,
-                        column5
-              FROM      table1
-              ) a
-    JOIN      table2 b
-    ON        a.column5 = b.column5
-    WHERE     column6
-    AND       column7
-    GROUP BY  column4;
-    `);
-
-  const tenSpaceRightResult = [
-    '   SELECT COUNT(a.column1),',
-    '          MAX(b.column2 + b.column3),',
-    '          b.column4 AS four',
-    '     FROM (',
-    '             SELECT column1,',
-    '                    column5',
-    '               FROM table1',
-    '          ) a',
-    '     JOIN table2 b',
-    '       ON a.column5 = b.column5',
-    '    WHERE column6',
-    '      AND column7',
-    ' GROUP BY column4;',
-  ].join('\n');
-
   it('supports standard mode', () => {
     const result = format(baseQuery, { keywordPosition: KeywordMode.standard });
-    expect(result).toBe(standardResult);
+    expect(result).toBe(dedent`
+      SELECT
+        COUNT(a.column1),
+        MAX(b.column2 + b.column3),
+        b.column4 AS four
+      FROM
+      (
+        SELECT
+          column1,
+          column5
+        FROM
+          table1
+      ) a
+      JOIN table2 b
+      ON a.column5 = b.column5
+      WHERE
+        column6
+        AND column7
+      GROUP BY
+        column4;
+    `);
   });
 
   it('supports tenSpaceLeft mode', () => {
     const result = format(baseQuery, { keywordPosition: KeywordMode.tenSpaceLeft });
-    expect(result).toBe(tenSpaceLeftResult);
+    expect(result).toBe(dedent`
+      SELECT    COUNT(a.column1),
+                MAX(b.column2 + b.column3),
+                b.column4 AS four
+      FROM      (
+                SELECT    column1,
+                          column5
+                FROM      table1
+                ) a
+      JOIN      table2 b
+      ON        a.column5 = b.column5
+      WHERE     column6
+      AND       column7
+      GROUP BY  column4;
+    `);
   });
 
   it('supports tenSpaceRight mode', () => {
     const result = format(baseQuery, { keywordPosition: KeywordMode.tenSpaceRight });
-    expect(result).toBe(tenSpaceRightResult);
+    expect(result).toBe(
+      [
+        '   SELECT COUNT(a.column1),',
+        '          MAX(b.column2 + b.column3),',
+        '          b.column4 AS four',
+        '     FROM (',
+        '             SELECT column1,',
+        '                    column5',
+        '               FROM table1',
+        '          ) a',
+        '     JOIN table2 b',
+        '       ON a.column5 = b.column5',
+        '    WHERE column6',
+        '      AND column7',
+        ' GROUP BY column4;',
+      ].join('\n')
+    );
   });
 
   it('handles long keyword', () => {
