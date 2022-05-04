7423b1ae91578cca9085fa9ecca3d4dc19494a19
Rene Saarsoo <nene@triin.net>
2022-05-04 14:49:37 +0300

    More structure for keywordPosition tests

--- test/options/keywordPosition.ts
+++ test/options/keywordPosition.ts
@@ -36,66 +36,95 @@ export default function supportsKeywordPositions(language: SqlLanguage, format: 
     `);
   });
 
-  it('supports tenSpaceLeft mode', () => {
-    const result = format(baseQuery, { keywordPosition: KeywordMode.tenSpaceLeft });
-    expect(result).toBe(dedent`
-      SELECT    COUNT(a.column1),
-                MAX(b.column2 + b.column3),
-                b.column4 AS four
-      FROM      (
-                SELECT    column1,
-                          column5
-                FROM      table1
-                ) a
-      JOIN      table2 b
-      ON        a.column5 = b.column5
-      WHERE     column6
-      AND       column7
-      GROUP BY  column4;
-    `);
+  describe('keywordPosition: tenSpaceLeft', () => {
+    it('aligns command keywords to left', () => {
+      const result = format(baseQuery, { keywordPosition: KeywordMode.tenSpaceLeft });
+      expect(result).toBe(dedent`
+        SELECT    COUNT(a.column1),
+                  MAX(b.column2 + b.column3),
+                  b.column4 AS four
+        FROM      (
+                  SELECT    column1,
+                            column5
+                  FROM      table1
+                  ) a
+        JOIN      table2 b
+        ON        a.column5 = b.column5
+        WHERE     column6
+        AND       column7
+        GROUP BY  column4;
+      `);
+    });
+
+    it('handles long keywords', () => {
+      expect(
+        format(
+          dedent`
+            SELECT *
+            FROM a
+            UNION DISTINCT
+            SELECT *
+            FROM b
+            LEFT OUTER JOIN c;
+          `,
+          { keywordPosition: KeywordMode.tenSpaceLeft }
+        )
+      ).toBe(dedent`
+        SELECT    *
+        FROM      a
+        UNION     DISTINCT
+        SELECT    *
+        FROM      b
+        LEFT      OUTER JOIN c;
+      `);
+    });
   });
 
-  it('supports tenSpaceRight mode', () => {
-    const result = format(baseQuery, { keywordPosition: KeywordMode.tenSpaceRight });
-    expect(result).toBe(
-      [
-        '   SELECT COUNT(a.column1),',
-        '          MAX(b.column2 + b.column3),',
-        '          b.column4 AS four',
-        '     FROM (',
-        '             SELECT column1,',
-        '                    column5',
-        '               FROM table1',
-        '          ) a',
-        '     JOIN table2 b',
-        '       ON a.column5 = b.column5',
-        '    WHERE column6',
-        '      AND column7',
-        ' GROUP BY column4;',
-      ].join('\n')
-    );
-  });
+  describe('keywordPosition: tenSpaceRight', () => {
+    it('aligns command keywords to right', () => {
+      const result = format(baseQuery, { keywordPosition: KeywordMode.tenSpaceRight });
+      expect(result).toBe(
+        [
+          '   SELECT COUNT(a.column1),',
+          '          MAX(b.column2 + b.column3),',
+          '          b.column4 AS four',
+          '     FROM (',
+          '             SELECT column1,',
+          '                    column5',
+          '               FROM table1',
+          '          ) a',
+          '     JOIN table2 b',
+          '       ON a.column5 = b.column5',
+          '    WHERE column6',
+          '      AND column7',
+          ' GROUP BY column4;',
+        ].join('\n')
+      );
+    });
 
-  it('handles long keyword', () => {
-    expect(
-      format(
-        dedent`
-          SELECT *
-          FROM a
-          UNION DISTINCT
-          SELECT *
-          FROM b
-          LEFT OUTER JOIN c;
-        `,
-        { keywordPosition: KeywordMode.tenSpaceLeft }
-      )
-    ).toBe(dedent`
-      SELECT    *
-      FROM      a
-      UNION     DISTINCT
-      SELECT    *
-      FROM      b
-      LEFT      OUTER JOIN c;
-    `);
+    it('handles long keywords', () => {
+      expect(
+        format(
+          dedent`
+            SELECT *
+            FROM a
+            UNION DISTINCT
+            SELECT *
+            FROM b
+            LEFT OUTER JOIN c;
+          `,
+          { keywordPosition: KeywordMode.tenSpaceRight }
+        )
+      ).toBe(
+        [
+          '   SELECT *',
+          '     FROM a',
+          '    UNION DISTINCT',
+          '   SELECT *',
+          '     FROM b',
+          '     LEFT OUTER JOIN c;',
+        ].join('\n')
+      );
+    });
   });
 }
