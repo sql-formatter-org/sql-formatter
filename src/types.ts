87c00d6733e238f59d6f907d9c70e9e5f2104323
Rene Saarsoo <nene@triin.net>
2022-05-08 20:24:08 +0300

    Drop enum docs
    Better to refer at the full documentation
    than to document everything again inside code.

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,5 @@
 import { ParamItems } from './core/Params';
 
-/**
- * Enum for the different keyword formats
- * @enum {string}
- * @property {string} standard - Standard keyword format
- * @property {string} tabularLeft - Central aligned keyword format, keywords left-aligned
- * @property {string} tabularRight - Central aligned keyword format, keywords right-aligned
- */
 export enum IndentStyle {
   standard = 'standard',
   tabularLeft = 'tabularLeft',
@@ -19,27 +12,12 @@ export enum KeywordCase {
   lower = 'lower',
 }
 
-/**
- * Enum for the different newline modes
- * @enum {string}
- * @property {string} always - Always use newlines
- * @property {string} never - Never use newlines
- * @property {string} lineWidth - Use newlines when line width is greater than the specified number
- */
 export enum NewlineMode {
   always = 'always',
   never = 'never',
   lineWidth = 'lineWidth',
 }
 
-/**
- * Enum for when to place AS for column aliases
- * @enum {string}
- * @property {string} preserve - Don't change, keep like in original
- * @property {string} always - Always use AS
- * @property {string} never - Never use AS
- * @property {string} select - Only use AS for SELECT clauses
- */
 export enum AliasMode {
   preserve = 'preserve',
   always = 'always',
@@ -47,13 +25,6 @@ export enum AliasMode {
   select = 'select',
 }
 
-/**
- * Enum for when to place commas in listed clauses
- * @enum {string}
- * @property {string} after - Place after each item
- * @property {string} before - Place before each item
- * @property {string} tabular - Place commas at end of line, right-justified
- */
 export enum CommaPosition {
   before = 'before',
   after = 'after',
