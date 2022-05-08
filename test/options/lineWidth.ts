a971f3685dcbc12e7beea3b194d7fbce9f217718
Rene Saarsoo <nene@triin.net>
2022-05-08 18:08:36 +0300

    Additional lineWidth tests

--- test/options/lineWidth.ts
+++ test/options/lineWidth.ts
@@ -18,7 +18,7 @@ export default function supportsLineWidth(language: SqlLanguage, format: FormatF
     );
   });
 
-  it('breaks paranthesize expressions to multiple lines when they exceed lineWidth', () => {
+  it('breaks paranthesized expressions to multiple lines when they exceed lineWidth', () => {
     const result = format('SELECT (price * tax) AS total FROM table_name WHERE (amount > 25);', {
       lineWidth: 10,
     });
@@ -33,6 +33,40 @@ export default function supportsLineWidth(language: SqlLanguage, format: FormatF
         (
           amount > 25
         );
+    `);
+  });
+
+  it('keeps paranthesized expressions on single lines when they do not exceed lineWidth', () => {
+    const result = format('SELECT (price * tax) AS total FROM table_name WHERE (amount > 25);', {
+      lineWidth: 11,
+    });
+    expect(result).toBe(dedent`
+      SELECT
+        (price * tax) AS total
+      FROM
+        table_name
+      WHERE
+        (amount > 25);
+    `);
+  });
+
+  // BUG: Spaces should be considered when computing expression length
+  it('calculates parenthesized expression length without considering spaces', () => {
+    const result = format('SELECT (price * tax) AS total FROM table_name WHERE (amount > 25);', {
+      lineWidth: 10,
+      denseOperators: true,
+    });
+    expect(result).toBe(dedent`
+    SELECT
+      (
+        price*tax
+      ) AS total
+    FROM
+      table_name
+    WHERE
+      (
+        amount>25
+      );
     `);
   });
 }
