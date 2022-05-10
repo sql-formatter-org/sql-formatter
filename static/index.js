92f8a571cb47bb2867fe4d91d2a35b22a9afce84
Rene Saarsoo <nene@triin.net>
2022-05-10 21:16:29 +0300

    Better error handling in demo page
    A separetely styled div for errors and special handling of
    configuration errors (which one shouldn't report as issues).

--- static/index.js
+++ static/index.js
@@ -1,10 +1,12 @@
 const attachFormat = () => {
+  const input = document.getElementById('input');
+  const output = document.getElementById('output');
+  const error = document.getElementById('error');
+
   const language = document.getElementById('language');
   const tabWidth = document.getElementById('tabWidth');
   const useTabs = document.getElementById('useTabs');
   const keywordCase = document.getElementById('keywordCase');
-  const input = document.getElementById('input');
-  const output = document.getElementById('output');
   const indentStyle = document.getElementById('indentStyle');
   const logicalOperatorNewline = document.getElementById('logicalOperatorNewline');
   const aliasAs = document.getElementById('aliasAs');
@@ -18,6 +20,18 @@ const attachFormat = () => {
   const lineBetweenQueries = document.getElementById('lineBetweenQueries');
   const denseOperators = document.getElementById('denseOperators');
   const newlineBeforeSemicolon = document.getElementById('newlineBeforeSemicolon');
+
+  function showOutput(text) {
+    output.value = text;
+    output.style.display = 'block';
+    error.style.display = 'none';
+  }
+
+  function showError(text) {
+    error.innerHTML = text;
+    output.style.display = 'none';
+    error.style.display = 'block';
+  }
 
   function format() {
     if (multilineLists.options[multilineLists.selectedIndex].value === 'itemCount') {
@@ -49,14 +63,24 @@ const attachFormat = () => {
         denseOperators: denseOperators.checked,
         newlineBeforeSemicolon: newlineBeforeSemicolon.checked,
       };
-      output.value = sqlFormatter.format(input.value, config);
+      showOutput(sqlFormatter.format(input.value, config));
     } catch (e) {
-      output.value = `
-An Error Occurred, please report this at:
-https://github.com/zeroturnaround/sql-formatter/issues\n
-Stack Trace:
-${e.stack.toString()}
-`;
+      if (e instanceof sqlFormatter.ConfigError) {
+        showError(`<h2>Configuration error</h2><p>${e.message}</p>`);
+      } else {
+        showError(
+          `
+<h2>An Unexpected Error Occurred</h2>
+<p><strong>${e.message}</strong></p>
+<p>
+  Please report this at
+  <a href="https://github.com/zeroturnaround/sql-formatter/issues">Github issues page.<a>
+</p>
+<p>Stack Trace:</p>
+<pre>${e.stack.toString()}</pre>
+`
+        );
+      }
     }
   }
 
