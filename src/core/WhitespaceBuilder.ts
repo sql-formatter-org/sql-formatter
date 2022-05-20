62f3574b57275ffed4408c8b506b3a2df9ddaec1
Rene Saarsoo <nene@triin.net>
2022-05-20 13:17:24 +0300

    Rewrite WhitespaceBuilder to not use string concatenation
    Should be more optimal.
    The tests now run about 2x faster.

--- src/core/WhitespaceBuilder.ts
+++ src/core/WhitespaceBuilder.ts
@@ -1,4 +1,3 @@
-import { trimSpacesEnd } from '../utils';
 import Indentation from './Indentation';
 
 /** Whitespace modifiers to be used with add() method */
@@ -14,11 +13,12 @@ export enum WS {
 /**
  * API for constructing SQL string (especially the whitespace part).
  *
- * It hides the internal implementation, so it can be rewritten to
- * use something more efficient than current string concatenation.
+ * It hides the internal implementation.
+ * Originally it used plain string concatenation, which was expensive.
+ * Now it's storing items to array and builds the string only in the end.
  */
 export default class WhitespaceBuilder {
-  private query = '';
+  private query: (WS.SPACE | WS.SINGLE_INDENT | WS.NEWLINE | string)[] = [];
 
   constructor(private indentation: Indentation) {}
 
@@ -29,33 +29,47 @@ export default class WhitespaceBuilder {
     for (const item of items) {
       switch (item) {
         case WS.SPACE:
-          this.query += ' ';
+          this.query.push(WS.SPACE);
           break;
         case WS.NO_SPACE:
-          this.query = trimSpacesEnd(this.query);
+          this.trimHorizontalWhitespace();
           break;
         case WS.NEWLINE:
+          this.trimHorizontalWhitespace();
           this.addNewline();
           break;
         case WS.NO_NEWLINE:
-          this.query = this.query.trimEnd();
+          this.trimAllWhitespace();
           break;
         case WS.INDENT:
-          this.query += this.indentation.getIndent();
+          for (let i = 0; i < this.indentation.getLevel(); i++) {
+            this.query.push(WS.SINGLE_INDENT);
+          }
           break;
         case WS.SINGLE_INDENT:
-          this.query += this.indentation.getSingleIndent();
+          this.query.push(WS.SINGLE_INDENT);
           break;
         default:
-          this.query += item;
+          this.query.push(item);
       }
     }
   }
 
+  private trimHorizontalWhitespace() {
+    while (isHorizontalWhitespace(last(this.query))) {
+      this.query.pop();
+    }
+  }
+
+  private trimAllWhitespace() {
+    while (isWhitespace(last(this.query))) {
+      this.query.pop();
+    }
+  }
+
   private addNewline() {
-    this.query = trimSpacesEnd(this.query);
-    if (!this.query.endsWith('\n') && this.query !== '') {
-      this.query += '\n';
+    if (this.query.length > 0 && last(this.query) !== WS.NEWLINE) {
+      this.query.push(WS.NEWLINE);
     }
   }
 
@@ -63,6 +77,27 @@ export default class WhitespaceBuilder {
    * Returns the final SQL string.
    */
   public toString(): string {
-    return this.query;
+    return this.query.map(item => this.itemToString(item)).join('');
+  }
+
+  private itemToString(item: WS | string) {
+    switch (item) {
+      case WS.SPACE:
+        return ' ';
+      case WS.NEWLINE:
+        return '\n';
+      case WS.SINGLE_INDENT:
+        return this.indentation.getSingleIndent();
+      default:
+        return item;
+    }
   }
 }
+
+const isHorizontalWhitespace = (item: WS | string | undefined) =>
+  item === WS.SPACE || item === WS.SINGLE_INDENT;
+
+const isWhitespace = (item: WS | string | undefined) =>
+  item === WS.SPACE || item === WS.SINGLE_INDENT || item === WS.NEWLINE;
+
+const last = <T>(arr: T[]): T | undefined => arr[arr.length - 1];
