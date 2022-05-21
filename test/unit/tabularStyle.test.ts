fffe1a7b5778a585b00718922ca66086b34bb69f
Rene Saarsoo <nene@triin.net>
2022-05-21 11:03:30 +0300

    Add unit tests for toTabularFormat() function

---
+++ test/unit/tabularStyle.test.ts
@@ -0,0 +1,21 @@
+import toTabularFormat from '../../src/core/tabularStyle';
+
+describe('toTabularFormat()', () => {
+  it('does nothing in standard style', () => {
+    expect(toTabularFormat('FROM', 'standard')).toBe('FROM');
+    expect(toTabularFormat('INNER JOIN', 'standard')).toBe('INNER JOIN');
+    expect(toTabularFormat('INSERT INTO', 'standard')).toBe('INSERT INTO');
+  });
+
+  it('formats in tabularLeft style', () => {
+    expect(toTabularFormat('FROM', 'tabularLeft')).toBe('FROM     ');
+    expect(toTabularFormat('INNER JOIN', 'tabularLeft')).toBe('INNER     JOIN');
+    expect(toTabularFormat('INSERT INTO', 'tabularLeft')).toBe('INSERT    INTO');
+  });
+
+  it('formats in tabularRight style', () => {
+    expect(toTabularFormat('FROM', 'tabularRight')).toBe('     FROM');
+    expect(toTabularFormat('INNER JOIN', 'tabularRight')).toBe('    INNER JOIN');
+    expect(toTabularFormat('INSERT INTO', 'tabularRight')).toBe('   INSERT INTO');
+  });
+});
