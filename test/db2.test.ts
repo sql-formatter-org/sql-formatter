eee0ea32218b0fcfe7247bf0cf4b049841d732ef
Rene Saarsoo <nene@triin.net>
2022-05-26 17:23:00 +0300

    Use generic string tests for DB2 N'' strings

--- test/db2.test.ts
+++ test/db2.test.ts
@@ -26,7 +26,7 @@ describe('Db2Formatter', () => {
   supportsConstraints(format);
   supportsAlterTable(format);
   supportsDeleteFrom(format);
-  supportsStrings(format, ["''", "X''", "U&''"]);
+  supportsStrings(format, ["''", "X''", "U&''", "N''"]);
   supportsIdentifiers(format, [`""`]);
   supportsBetween(format);
   supportsSchema(format);
@@ -74,11 +74,10 @@ describe('Db2Formatter', () => {
   });
 
   // DB2-specific string types
-  it('supports strings with G, N, GX, UX prefixes', () => {
-    expect(format(`SELECT G'blah blah', N'hah haa', GX'01AC', UX'CCF239' FROM foo`)).toBe(dedent`
+  it('supports strings with G, GX, UX prefixes', () => {
+    expect(format(`SELECT G'blah blah', GX'01AC', UX'CCF239' FROM foo`)).toBe(dedent`
       SELECT
         G'blah blah',
-        N'hah haa',
         GX'01AC',
         UX'CCF239'
       FROM
