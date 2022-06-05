dd2ec3b124f7306144a57dfdcedc199f24b32440
Rene Saarsoo <nene@triin.net>
2022-06-05 21:59:57 +0300

    Use absolute path import

--- test/features/identifiers.ts
+++ test/features/identifiers.ts
@@ -1,6 +1,6 @@
 import { expect } from '@jest/globals';
 import dedent from 'dedent-js';
-import { FormatFn } from '../../src/sqlFormatter';
+import { FormatFn } from 'src/sqlFormatter';
 
 type IdentType = '""' | '``' | '[]' | 'U&""';
 
