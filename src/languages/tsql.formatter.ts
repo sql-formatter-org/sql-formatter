eb06103a356a1efe21dc8d0d794c15c59fd73fb4
inferrinizzard <inferrinizzard@gmail.com>
2022-07-07 22:40:19 -0700

    update tsql operators

--- src/languages/tsql.formatter.ts
+++ src/languages/tsql.formatter.ts
@@ -1227,7 +1227,20 @@ const reservedDependentClauses = ['WHEN', 'ELSE'];
 
 // https://docs.microsoft.com/en-us/sql/t-sql/language-reference?view=sql-server-ver15
 export default class TSqlFormatter extends Formatter {
-  static operators = ['!<', '!>', '+=', '-=', '*=', '/=', '%=', '|=', '&=', '^=', '::'];
+  static operators = [
+    '~',
+    '!<',
+    '!>',
+    '+=',
+    '-=',
+    '*=',
+    '/=',
+    '%=',
+    '|=',
+    '&=',
+    '^=',
+    // '::'
+  ];
 
   tokenizer() {
     return new Tokenizer({
