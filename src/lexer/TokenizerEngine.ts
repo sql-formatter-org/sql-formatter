e4a4cfa9a1af16def098f79fa3ad6d0c9da7a430
Rene Saarsoo <nene@triin.net>
2022-07-14 17:07:18 +0300

    Prioritize AND/OR/XOR over other keywords

--- src/lexer/TokenizerEngine.ts
+++ src/lexer/TokenizerEngine.ts
@@ -111,8 +111,8 @@ export default class TokenizerEngine {
       this.matchToken(TokenType.RESERVED_DEPENDENT_CLAUSE) ||
       this.matchToken(TokenType.RESERVED_JOIN) ||
       this.matchToken(TokenType.RESERVED_FUNCTION_NAME) ||
-      this.matchToken(TokenType.RESERVED_KEYWORD) ||
       this.matchToken(TokenType.RESERVED_LOGICAL_OPERATOR) ||
+      this.matchToken(TokenType.RESERVED_KEYWORD) ||
       this.matchToken(TokenType.RESERVED_JOIN_CONDITION)
     );
   }
