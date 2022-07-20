2774cc0959b973bf29161722f74a6912ec85cc07
inferrinizzard <inferrinizzard@gmail.com>
2022-07-19 22:09:31 -0700

    add prefixes for double quotes in mariadb

--- src/languages/mariadb/mariadb.formatter.ts
+++ src/languages/mariadb/mariadb.formatter.ts
@@ -269,7 +269,10 @@ export default class MariaDbFormatter extends Formatter {
       reservedKeywords: keywords,
       reservedFunctionNames: functions,
       // TODO: support _ char set prefixes such as _utf8, _latin1, _binary, _utf8mb4, etc.
-      stringTypes: ['""', { quote: "''", prefixes: ['B', 'X'] }],
+      stringTypes: [
+        { quote: "''", prefixes: ['B', 'X'] },
+        { quote: '""', prefixes: ['B', 'X'] },
+      ],
       identTypes: ['``'],
       identChars: { first: '$', rest: '$' },
       variableTypes: [
