87b9b54d4fcd52744a5a8c05dded2d77bb47ec04
inferrinizzard <inferrinizzard@gmail.com>
2022-07-23 12:03:36 -0700

    rewrite string tests to include prefixes and escapes

--- test/features/strings.ts
+++ test/features/strings.ts
@@ -1,106 +1,39 @@
 import { expect } from '@jest/globals';
-import dedent from 'dedent-js';
+// import dedent from 'dedent-js';
 
 import { FormatFn } from 'src/sqlFormatter';
+import type { QuoteType } from 'src/lexer/regexTypes';
 
-type StringType = '""' | "''" | 'U&""' | "U&''" | "N''" | "X''";
+export default function supportsStrings(format: FormatFn, stringTypes: QuoteType[]) {
+  const baseTestString = 'foo JOIN bar';
 
-export default function supportsStrings(format: FormatFn, stringTypes: StringType[]) {
-  if (stringTypes.includes('""')) {
-    it('supports double-quoted strings', () => {
-      expect(format('"foo JOIN bar"')).toBe('"foo JOIN bar"');
-      expect(format('"foo \\" JOIN bar"')).toBe('"foo \\" JOIN bar"');
-      expect(format('SELECT "where" FROM "update"')).toBe(dedent`
-        SELECT
-          "where"
-        FROM
-          "update"
-      `);
-    });
+  // for each valid string type
+  for (const quoteType of stringTypes) {
+    const quoteString = typeof quoteType === 'string' ? quoteType : quoteType.quote;
+    it.only(`supports ${quoteString} strings`, () => {
+      const testString = [
+        quoteString.slice(0, quoteString.length / 2).replace(/[.]/g, ''),
+        baseTestString,
+        quoteString.slice(quoteString.length / 2).replace(/[.]/g, ''),
+      ].join('');
 
-    it('supports escaping double-quote by doubling it', () => {
-      expect(format('"foo""bar"')).toBe('"foo""bar"');
-    });
-  }
+      expect(format(testString)).toBe(testString);
 
-  if (stringTypes.includes("''")) {
-    it('supports single-quoted strings', () => {
-      expect(format("'foo JOIN bar'")).toBe("'foo JOIN bar'");
-      expect(format("'foo \\' JOIN bar'")).toBe("'foo \\' JOIN bar'");
-      expect(format("SELECT 'where' FROM 'update'")).toBe(dedent`
-        SELECT
-          'where'
-        FROM
-          'update'
-      `);
-    });
+      // for complex quote types
+      if (typeof quoteType !== 'string') {
+        if (quoteType.escapes) {
+          for (const escape of quoteType.escapes) {
+            expect(testString.replace('JOIN', escape)).toBe(testString.replace('JOIN', escape));
+          }
+        }
 
-    it('supports escaping single-quote by doubling it', () => {
-      expect(format("'foo''bar'")).toBe("'foo''bar'");
-    });
-  }
-
-  if (stringTypes.includes('U&""')) {
-    it('supports unicode double-quoted strings', () => {
-      expect(format('U&"foo JOIN bar"')).toBe('U&"foo JOIN bar"');
-      expect(format('U&"foo \\" JOIN bar"')).toBe('U&"foo \\" JOIN bar"');
-      expect(format('SELECT U&"where" FROM U&"update"')).toBe(dedent`
-        SELECT
-          U&"where"
-        FROM
-          U&"update"
-      `);
-    });
-
-    it("detects consecutive U&'' strings as separate ones", () => {
-      expect(format("U&'foo'U&'bar'")).toBe("U&'foo' U&'bar'");
-    });
-  }
-
-  if (stringTypes.includes("U&''")) {
-    it('supports single-quoted strings', () => {
-      expect(format("U&'foo JOIN bar'")).toBe("U&'foo JOIN bar'");
-      expect(format("U&'foo \\' JOIN bar'")).toBe("U&'foo \\' JOIN bar'");
-      expect(format("SELECT U&'where' FROM U&'update'")).toBe(dedent`
-        SELECT
-          U&'where'
-        FROM
-          U&'update'
-      `);
-    });
-  }
-
-  if (stringTypes.includes("N''")) {
-    it('supports T-SQL unicode strings', () => {
-      expect(format("N'foo JOIN bar'")).toBe("N'foo JOIN bar'");
-      expect(format("N'foo \\' JOIN bar'")).toBe("N'foo \\' JOIN bar'");
-      expect(format("SELECT N'where' FROM N'update'")).toBe(dedent`
-        SELECT
-          N'where'
-        FROM
-          N'update'
-      `);
-    });
-
-    it("detects consecutive N'' strings as separate ones", () => {
-      expect(format("N'foo'N'bar'")).toBe("N'foo' N'bar'");
-    });
-  }
-
-  if (stringTypes.includes("X''")) {
-    it('supports hex byte sequences', () => {
-      expect(format("x'0E'")).toBe("x'0E'");
-      expect(format("X'1F0A89C3'")).toBe("X'1F0A89C3'");
-      expect(format("SELECT x'2B' FROM foo")).toBe(dedent`
-        SELECT
-          x'2B'
-        FROM
-          foo
-      `);
-    });
-
-    it("detects consecutive X'' strings as separate ones", () => {
-      expect(format("X'AE01'X'01F6'")).toBe("X'AE01' X'01F6'");
+        // for prefixed quote types
+        if ('prefixes' in quoteType) {
+          for (const prefix of quoteType.prefixes) {
+            expect(`${prefix}${testString}`).toBe(`${prefix}${testString}`);
+          }
+        }
+      }
     });
   }
 }
