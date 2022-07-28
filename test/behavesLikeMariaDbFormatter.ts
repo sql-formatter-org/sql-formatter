e92cd2e35b01815a5135a55cc8f50ee1bac26022
inferrinizzard <inferrinizzard@gmail.com>
2022-07-28 08:07:43 -0700

    update firstCharNumber test

--- test/behavesLikeMariaDbFormatter.ts
+++ test/behavesLikeMariaDbFormatter.ts
@@ -49,29 +49,12 @@ export default function behavesLikeMariaDbFormatter(format: FormatFn) {
 
   // regression test for sql-formatter#334
   it('supports identifiers that start with numbers', () => {
-    expect(format('SELECT 4four FROM tbl')).toBe(
+    expect(format('SELECT 4four, 12345e, 12e45 FROM tbl')).toBe(
       dedent`
         SELECT
-          4four
-        FROM
-          tbl
-      `
-    );
-    expect(format('SELECT 1, two, 3four FROM tbl')).toBe(
-      dedent`
-        SELECT
-          1,
-          two,
-          3four
-        FROM
-          tbl
-      `
-    );
-    expect(format('SELECT one + 2three, 4 + 5six FROM tbl')).toBe(
-      dedent`
-        SELECT
-          one + 2three,
-          4 + 5six
+          4four,
+          12345e,
+          12e45
         FROM
           tbl
       `
