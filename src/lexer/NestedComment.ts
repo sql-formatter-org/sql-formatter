6101d075f02f43682841ec66698cab71930652e7
Rene Saarsoo <nene@triin.net>
2022-09-01 15:35:27 +0300

    Implement NestedComment class

---
+++ src/lexer/NestedComment.ts
@@ -0,0 +1,52 @@
+/* eslint-disable no-cond-assign */
+import { RegExpLike } from 'src/lexer/TokenizerEngine';
+
+const START = /\/\*/uy; // matches: /*
+const MIDDLE = /([^/*]|\*[^/]|\/[^*])+/uy; // matches text NOT containing /* or */
+const END = /\*\//uy; // matches: */
+
+/**
+ * An object mimicking a regular expression,
+ * for matching nested block-comments.
+ */
+export class NestedComment implements RegExpLike {
+  public lastIndex: number = 0;
+
+  public exec(input: string): string[] | null {
+    let result = '';
+    let match: string | null;
+    let nestLevel = 0;
+
+    if ((match = this.matchSection(START, input))) {
+      result += match;
+      nestLevel++;
+    } else {
+      return null;
+    }
+
+    while (nestLevel > 0) {
+      if ((match = this.matchSection(START, input))) {
+        result += match;
+        nestLevel++;
+      } else if ((match = this.matchSection(END, input))) {
+        result += match;
+        nestLevel--;
+      } else if ((match = this.matchSection(MIDDLE, input))) {
+        result += match;
+      } else {
+        return null;
+      }
+    }
+
+    return [result];
+  }
+
+  private matchSection(regex: RegExp, input: string): string | null {
+    regex.lastIndex = this.lastIndex;
+    const matches = regex.exec(input);
+    if (matches) {
+      this.lastIndex += matches[0].length;
+    }
+    return matches ? matches[0] : null;
+  }
+}
---
+++ test/unit/NestedComment.test.ts
@@ -0,0 +1,63 @@
+import { NestedComment } from 'src/lexer/NestedComment';
+
+describe('NestedComment', () => {
+  const match = (input: string, index: number) => {
+    const re = new NestedComment();
+    re.lastIndex = index;
+    return re.exec(input);
+  };
+
+  it('matches comment at the start of a string', () => {
+    expect(match('/* comment */ blah...', 0)).toEqual(['/* comment */']);
+  });
+
+  it('matches empty comment block', () => {
+    expect(match('/**/ blah...', 0)).toEqual(['/**/']);
+  });
+
+  it('matches comment containing * and / characters', () => {
+    expect(match('/** // */ blah...', 0)).toEqual(['/** // */']);
+  });
+
+  it('matches only first comment, when two comments in row', () => {
+    expect(match('/*com1*//*com2*/ blah...', 0)).toEqual(['/*com1*/']);
+  });
+
+  it('matches comment in the middle of a string', () => {
+    expect(match('hello /* comment */ blah...', 6)).toEqual(['/* comment */']);
+  });
+
+  it('does not match a comment when index not set to its start position', () => {
+    expect(match('hello /* comment */ blah...', 1)).toEqual(null);
+  });
+
+  it('does not match unclosed comment', () => {
+    expect(match('/* comment blah...', 0)).toEqual(null);
+  });
+
+  it('does not match unopened comment', () => {
+    expect(match(' comment */ blah...', 0)).toEqual(null);
+  });
+
+  it('matches a nested comment', () => {
+    expect(match('/* some /* nested */ comment */ blah...', 0)).toEqual([
+      '/* some /* nested */ comment */',
+    ]);
+  });
+
+  it('matches a multi-level nested comment', () => {
+    expect(match('/* some /* /* nested */ */ comment */ blah...', 0)).toEqual([
+      '/* some /* /* nested */ */ comment */',
+    ]);
+  });
+
+  it('matches multiple nested comments', () => {
+    expect(match('/* some /* n1 */ and /* n2 */ coms */ blah...', 0)).toEqual([
+      '/* some /* n1 */ and /* n2 */ coms */',
+    ]);
+  });
+
+  it('does not match an inproperly nested comment', () => {
+    expect(match('/* some /* comment blah...', 0)).toEqual(null);
+  });
+});
