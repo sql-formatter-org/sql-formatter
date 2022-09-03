e02811157a8b5d359a720b8d445cadf3935e7783
Rene Saarsoo <nene@triin.net>
2022-09-03 17:19:29 +0300

    Additional quote patterns with escape-style modifiers

--- src/lexer/regexFactory.ts
+++ src/lexer/regexFactory.ts
@@ -110,7 +110,13 @@ export const quotePatterns = {
   '``': '(?:`[^`]*`)+',
   '[]': String.raw`(?:\[[^\]]*\])(?:\][^\]]*\])*`,
   '""': String.raw`(?:"[^"\\]*(?:\\.[^"\\]*)*")+`,
+  '""-qq': String.raw`(?:"[^"]*")+`, // with repeated quote escapes
+  '""-bs': String.raw`(?:"[^"\\]*(?:\\.[^"\\]*)*")`, // with backslash escapes
+  '""-qq-bs': String.raw`(?:"[^"\\]*(?:\\.[^"\\]*)*")+`, // with repeated quote or backslash escapes
   "''": String.raw`(?:'[^'\\]*(?:\\.[^'\\]*)*')+`,
+  "''-qq": String.raw`(?:'[^']*')+`, // with repeated quote escapes
+  "''-bs": String.raw`(?:'[^'\\]*(?:\\.[^'\\]*)*')`, // with backslash escapes
+  "''-qq-bs": String.raw`(?:'[^'\\]*(?:\\.[^'\\]*)*')+`, // with repeated quote or backslash escapes
   '$$': String.raw`(?<tag>\$\w*\$)[\s\S]*?\k<tag>`,
   "'''..'''": String.raw`'''[^\\]*?(?:\\.[^\\]*?)*?'''`,
   '""".."""': String.raw`"""[^\\]*?(?:\\.[^\\]*?)*?"""`,
