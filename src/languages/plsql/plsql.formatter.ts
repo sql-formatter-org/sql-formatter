9bb007a1ceb13408dbffc28fca3fabcc175835cd
Rene Saarsoo <nene@triin.net>
2022-09-03 21:50:45 +0300

    Document PL/SQL double-quoted identifiers special case

--- src/languages/plsql/plsql.formatter.ts
+++ src/languages/plsql/plsql.formatter.ts
@@ -106,6 +106,9 @@ export default class PlSqlFormatter extends Formatter {
         { quote: "''-qq", prefixes: ['N'] },
         { quote: "q''", prefixes: ['N'] },
       ],
+      // PL/SQL doesn't actually support escaping of quotes in identifiers,
+      // but for the sake of simpler testing we'll support this anyway
+      // as all other SQL dialects with "identifiers" do.
       identTypes: [`""-qq`],
       identChars: { rest: '$#' },
       variableTypes: [{ regex: '&{1,2}[A-Za-z][A-Za-z0-9_$#]*' }],
