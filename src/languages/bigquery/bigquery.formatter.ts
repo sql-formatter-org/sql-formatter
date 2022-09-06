a1d06da638c6a05986021e6dc2ce40bac525dea4
Rene Saarsoo <nene@triin.net>
2022-09-06 15:18:55 +0300

    Group start-end data to single loc field in Token object

--- src/languages/bigquery/bigquery.formatter.ts
+++ src/languages/bigquery/bigquery.formatter.ts
@@ -204,8 +204,10 @@ function combineParameterizedTypes(tokens: Token[]) {
         type: TokenType.IDENTIFIER,
         raw: typeDefTokens.map(formatTypeDefToken('raw')).join(''),
         text: typeDefTokens.map(formatTypeDefToken('text')).join(''),
-        start: token.start,
-        end: token.end + typeDefTokens.map(t => t.text.length).reduce((a, b) => a + b),
+        loc: {
+          start: token.loc.start,
+          end: token.loc.end + typeDefTokens.map(t => t.text.length).reduce((a, b) => a + b),
+        },
       });
       i = endIndex;
     } else {
