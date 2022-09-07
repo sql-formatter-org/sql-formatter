5d3bbcb3303f307af47371dc679ff36dedbbd0a5
Rene Saarsoo <nene@triin.net>
2022-09-07 19:41:49 +0300

    Add optional comment fields to all Nodes

--- src/parser/ast.ts
+++ src/parser/ast.ts
@@ -21,38 +21,43 @@ export enum NodeType {
   block_comment = 'block_comment',
 }
 
-export type StatementNode = {
+type BaseNode = {
+  leadingComments?: CommentNode[];
+  trailingComments?: CommentNode[];
+};
+
+export type StatementNode = BaseNode & {
   type: NodeType.statement;
   children: AstNode[];
   hasSemicolon: boolean;
 };
 
-export type ClauseNode = {
+export type ClauseNode = BaseNode & {
   type: NodeType.clause;
   name: KeywordNode;
   children: AstNode[];
 };
 
-export type SetOperationNode = {
+export type SetOperationNode = BaseNode & {
   type: NodeType.set_operation;
   name: KeywordNode;
   children: AstNode[];
 };
 
-export type FunctionCallNode = {
+export type FunctionCallNode = BaseNode & {
   type: NodeType.function_call;
   name: KeywordNode;
   parenthesis: ParenthesisNode;
 };
 
 // <ident>[<expr>]
-export type ArraySubscriptNode = {
+export type ArraySubscriptNode = BaseNode & {
   type: NodeType.array_subscript;
   array: IdentifierNode | KeywordNode;
   parenthesis: ParenthesisNode;
 };
 
-export type ParenthesisNode = {
+export type ParenthesisNode = BaseNode & {
   type: NodeType.parenthesis;
   children: AstNode[];
   openParen: string;
@@ -60,7 +65,7 @@ export type ParenthesisNode = {
 };
 
 // BETWEEN <expr1> AND <expr2>
-export type BetweenPredicateNode = {
+export type BetweenPredicateNode = BaseNode & {
   type: NodeType.between_predicate;
   between: KeywordNode;
   expr1: AstNode[];
@@ -70,7 +75,7 @@ export type BetweenPredicateNode = {
 
 // LIMIT <count>
 // LIMIT <offset>, <count>
-export type LimitClauseNode = {
+export type LimitClauseNode = BaseNode & {
   type: NodeType.limit_clause;
   name: KeywordNode;
   count: AstNode[];
@@ -78,58 +83,60 @@ export type LimitClauseNode = {
 };
 
 // The "*" operator used in SELECT *
-export type AllColumnsAsteriskNode = {
+export type AllColumnsAsteriskNode = BaseNode & {
   type: NodeType.all_columns_asterisk;
 };
 
-export type LiteralNode = {
+export type LiteralNode = BaseNode & {
   type: NodeType.literal;
   text: string;
 };
 
-export type PropertyAccessNode = {
+export type PropertyAccessNode = BaseNode & {
   type: NodeType.property_access;
   object: AstNode;
   property: IdentifierNode | ArraySubscriptNode | AllColumnsAsteriskNode;
 };
 
-export type IdentifierNode = {
+export type IdentifierNode = BaseNode & {
   type: NodeType.identifier;
   text: string;
 };
 
-export type KeywordNode = {
+export type KeywordNode = BaseNode & {
   type: NodeType.keyword;
   tokenType: TokenType;
   text: string;
   raw: string;
 };
 
-export type ParameterNode = {
+export type ParameterNode = BaseNode & {
   type: NodeType.parameter;
   key?: string;
   text: string;
 };
 
-export type OperatorNode = {
+export type OperatorNode = BaseNode & {
   type: NodeType.operator;
   text: string;
 };
 
-export type CommaNode = {
+export type CommaNode = BaseNode & {
   type: NodeType.comma;
 };
 
-export type LineCommentNode = {
+export type LineCommentNode = BaseNode & {
   type: NodeType.line_comment;
   text: string;
   precedingWhitespace: string;
 };
 
-export type BlockCommentNode = {
+export type BlockCommentNode = BaseNode & {
   type: NodeType.block_comment;
   text: string;
 };
+
+export type CommentNode = LineCommentNode | BlockCommentNode;
 
 export type AstNode =
   | ClauseNode
