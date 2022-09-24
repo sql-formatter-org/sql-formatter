d358feb6d6154d1c561fa072fac556a2c51ed639
inferrinizzard <inferrinizzard@gmail.com>
2022-09-23 20:42:32 -0700

    remove exports

--- src/exports/BigQuerySQLFormatter.ts
+++
@@ -1,5 +0,0 @@
-import { format as baseFormat } from 'src/sqlFormatter';
-import type { FormatOptions } from 'src/FormatOptions';
-
-export const format = (query: string, options: Exclude<FormatOptions, 'language'>) =>
-  baseFormat(query, { ...options, language: 'bigquery' });
