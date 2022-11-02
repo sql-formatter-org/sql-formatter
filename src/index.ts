f8cf3eb0837ae3134e31fe9fb0e999009d4e45e3
Rene Saarsoo <nene@triin.net>
2022-11-02 14:46:17 +0200

    Remove type exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,14 @@ export {
   format,
   formatDialect,
 } from './sqlFormatter.js';
-export type {
+export {
   IndentStyle,
   KeywordCase,
   CommaPosition,
   LogicalOperatorNewline,
   FormatOptions,
 } from './FormatOptions.js';
-export type { DialectOptions } from './dialect.js';
+export { DialectOptions } from './dialect.js';
 export { ConfigError } from './validateConfig.js';
 export { expandPhrases } from './expandPhrases.js';
 export * from './allDialects.js';
