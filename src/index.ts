6329fad24c0dcfa3d823632441f9e071bc2c427c
Rene Saarsoo <nene@triin.net>
2022-11-02 15:05:46 +0200

    Clarify usage of export *

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export { supportedDialects, format, formatDialect } from './sqlFormatter.js';
 export { expandPhrases } from './expandPhrases.js';
+
+// Intentionally use "export *" syntax here to make sure when adding a new SQL dialect
+// we wouldn't forget to expose it in our public API.
 export * from './allDialects.js';
 
 // NB! To re-export types the "export type" syntax is required by webpack.
