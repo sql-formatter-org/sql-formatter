b7b1643d2b72311bd8e6773c942c48ac1d13f0e5
Rene Saarsoo <nene@triin.net>
2023-11-10 12:06:34 +0200

    Generalize duplicated DB2 test code to behavesLikeDb2Formatter()

---
+++ test/behavesLikeDb2Formatter.ts
@@ -0,0 +1,134 @@
+import dedent from 'dedent-js';
+
+import { FormatFn } from '../src/sqlFormatter.js';
+import behavesLikeSqlFormatter from './behavesLikeSqlFormatter.js';
+
+import supportsAlterTable from './features/alterTable.js';
+import supportsBetween from './features/between.js';
+import supportsOperators from './features/operators.js';
+import supportsSchema from './features/schema.js';
+import supportsStrings from './features/strings.js';
+import supportsConstraints from './features/constraints.js';
+import supportsDeleteFrom from './features/deleteFrom.js';
+import supportsComments from './features/comments.js';
+import supportsCommentOn from './features/commentOn.js';
+import supportsIdentifiers from './features/identifiers.js';
+import supportsParams from './options/param.js';
+import supportsSetOperations from './features/setOperations.js';
+import supportsLimiting from './features/limiting.js';
+import supportsInsertInto from './features/insertInto.js';
+import supportsUpdate from './features/update.js';
+import supportsTruncateTable from './features/truncateTable.js';
+import supportsMergeInto from './features/mergeInto.js';
+import supportsCreateView from './features/createView.js';
+
+/**
+ * Shared tests for DB2 and DB2i
+ */
+export default function behavesLikeDb2Formatter(format: FormatFn) {
+  behavesLikeSqlFormatter(format);
+  supportsComments(format);
+  supportsCommentOn(format);
+  supportsCreateView(format, { orReplace: true });
+  supportsConstraints(format, ['NO ACTION', 'RESTRICT', 'CASCADE', 'SET NULL']);
+  supportsAlterTable(format, {
+    addColumn: true,
+    dropColumn: true,
+    renameColumn: true,
+  });
+  supportsDeleteFrom(format);
+  supportsInsertInto(format);
+  supportsUpdate(format, { whereCurrentOf: true });
+  supportsTruncateTable(format, { withoutTable: true });
+  supportsMergeInto(format);
+  supportsStrings(format, ["''-qq", "X''", "U&''", "N''"]);
+  supportsIdentifiers(format, [`""-qq`]);
+  supportsBetween(format);
+  supportsSchema(format);
+  supportsOperators(format, ['**', '¬=', '¬>', '¬<', '!>', '!<', '||']);
+  supportsSetOperations(format, [
+    'UNION',
+    'UNION ALL',
+    'EXCEPT',
+    'EXCEPT ALL',
+    'INTERSECT',
+    'INTERSECT ALL',
+  ]);
+  supportsParams(format, { positional: true, named: [':'] });
+  supportsLimiting(format, { fetchFirst: true });
+
+  it('formats only -- as a line comment', () => {
+    const result = format(`
+      SELECT col FROM
+      -- This is a comment
+      MyTable;
+    `);
+    expect(result).toBe(dedent`
+      SELECT
+        col
+      FROM
+        -- This is a comment
+        MyTable;
+    `);
+  });
+
+  // DB2-specific string types
+  it('supports strings with G, GX, BX, UX prefixes', () => {
+    expect(format(`SELECT G'blah blah', GX'01AC', BX'0101', UX'CCF239' FROM foo`)).toBe(dedent`
+      SELECT
+        G'blah blah',
+        GX'01AC',
+        BX'0101',
+        UX'CCF239'
+      FROM
+        foo
+    `);
+  });
+
+  it('supports @, #, $ characters at the start of identifiers', () => {
+    expect(format(`SELECT @foo, #bar, $zap`)).toBe(dedent`
+      SELECT
+        @foo,
+        #bar,
+        $zap
+    `);
+  });
+
+  it('supports @, #, $ characters in named parameters', () => {
+    expect(format(`SELECT :foo@bar, :foo#bar, :foo$bar, :@zip, :#zap, :$zop`)).toBe(dedent`
+      SELECT
+        :foo@bar,
+        :foo#bar,
+        :foo$bar,
+        :@zip,
+        :#zap,
+        :$zop
+    `);
+  });
+
+  it('supports WITH isolation level modifiers for UPDATE statement', () => {
+    expect(format('UPDATE foo SET x = 10 WITH CS')).toBe(dedent`
+      UPDATE foo
+      SET
+        x = 10
+      WITH CS
+    `);
+  });
+
+  it('formats ALTER TABLE ... ALTER COLUMN', () => {
+    expect(
+      format(
+        `ALTER TABLE t ALTER COLUMN foo SET DATA TYPE VARCHAR;
+         ALTER TABLE t ALTER COLUMN foo SET NOT NULL;`
+      )
+    ).toBe(dedent`
+      ALTER TABLE t
+      ALTER COLUMN foo
+      SET DATA TYPE VARCHAR;
+
+      ALTER TABLE t
+      ALTER COLUMN foo
+      SET NOT NULL;
+    `);
+  });
+}
