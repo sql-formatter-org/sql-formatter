3b1cd758df64c44666bad235dcc78f1a143f0b36
Rene Saarsoo <nene@triin.net>
2023-12-07 11:31:38 +0200

    Include parameterized types to dataTypeCase test

--- test/options/dataTypeCase.ts
+++ test/options/dataTypeCase.ts
@@ -4,27 +4,44 @@ import { FormatFn } from '../../src/sqlFormatter.js';
 
 export default function supportsDataTypeCase(format: FormatFn) {
   it('preserves data type keyword case by default', () => {
-    const result = format('CREATE TABLE users ( id iNt PRIMARY KEY )');
+    const result = format(
+      'CREATE TABLE users ( user_id iNt PRIMARY KEY, total_earnings Decimal(5, 2) NOT NULL )'
+    );
     expect(result).toBe(dedent`
-      CREATE TABLE users (id iNt PRIMARY KEY)
+      CREATE TABLE users (
+        user_id iNt PRIMARY KEY,
+        total_earnings Decimal(5, 2) NOT NULL
+      )
     `);
   });
 
   it('converts data type keyword case to uppercase', () => {
-    const result = format('CREATE TABLE users ( id iNt PRIMARY KEY )', {
-      dataTypeCase: 'upper',
-    });
+    const result = format(
+      'CREATE TABLE users ( user_id iNt PRIMARY KEY, total_earnings Decimal(5, 2) NOT NULL )',
+      {
+        dataTypeCase: 'upper',
+      }
+    );
     expect(result).toBe(dedent`
-      CREATE TABLE users (id INT PRIMARY KEY)
+      CREATE TABLE users (
+        user_id INT PRIMARY KEY,
+        total_earnings DECIMAL(5, 2) NOT NULL
+      )
     `);
   });
 
   it('converts data type keyword case to lowercase', () => {
-    const result = format('CREATE TABLE users ( id iNt PRIMARY KEY )', {
-      dataTypeCase: 'lower',
-    });
+    const result = format(
+      'CREATE TABLE users ( user_id iNt PRIMARY KEY, total_earnings Decimal(5, 2) NOT NULL )',
+      {
+        dataTypeCase: 'lower',
+      }
+    );
     expect(result).toBe(dedent`
-      CREATE TABLE users (id int PRIMARY KEY)
+      CREATE TABLE users (
+        user_id int PRIMARY KEY,
+        total_earnings decimal(5, 2) NOT NULL
+      )
     `);
   });
 }
