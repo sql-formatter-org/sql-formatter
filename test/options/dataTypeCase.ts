82b60bca0e5f3ab7e53d0fb642e8920525883320
Rene Saarsoo <nene@triin.net>
2023-12-07 11:18:53 +0200

    Remove unnecessary dataTypeCase tests (using CAST)

--- test/options/dataTypeCase.ts
+++ test/options/dataTypeCase.ts
@@ -27,38 +27,4 @@ export default function supportsDataTypeCase(format: FormatFn) {
       CREATE TABLE users (id int PRIMARY KEY)
     `);
   });
-
-  it('preserves data type keyword case in cast by default', () => {
-    const result = format('SELECT CAST(quantity AS InT) FROM orders');
-    expect(result).toBe(dedent`
-      SELECT
-        CAST(quantity AS InT)
-      FROM
-        orders
-    `);
-  });
-
-  it('converts data type keyword case in cast to uppercase', () => {
-    const result = format('SELECT CAST(quantity AS InT) FROM orders', {
-      dataTypeCase: 'upper',
-    });
-    expect(result).toBe(dedent`
-      SELECT
-        CAST(quantity AS INT)
-      FROM
-        orders
-    `);
-  });
-
-  it('converts data type keyword case in cast to lowercase', () => {
-    const result = format('SELECT CAST(quantity AS InT) FROM orders', {
-      dataTypeCase: 'lower',
-    });
-    expect(result).toBe(dedent`
-      SELECT
-        CAST(quantity AS int)
-      FROM
-        orders
-    `);
-  });
 }
