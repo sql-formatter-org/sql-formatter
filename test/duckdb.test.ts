89fc28b923b3b8c4312f8dd7672004a4d35db44a
Rene Saarsoo <nene@triin.net>
2025-04-20 18:06:13 +0300

    Extract behavesLikePostgresqlFormatter()

--- test/duckdb.test.ts
+++ test/duckdb.test.ts
@@ -3,6 +3,7 @@ import dedent from 'dedent-js';
 import { format as originalFormat, FormatFn } from '../src/sqlFormatter.js';
 
 import behavesLikeSqlFormatter from './behavesLikeSqlFormatter.js';
+import behavesLikePostgresqlFormatter from './behavesLikePostgresqlFormatter.js';
 import supportsAlterTable from './features/alterTable.js';
 import supportsBetween from './features/between.js';
 import supportsCreateTable from './features/createTable.js';
@@ -34,6 +35,7 @@ describe('DuckDBFormatter', () => {
   const format: FormatFn = (query, cfg = {}) => originalFormat(query, { ...cfg, language });
 
   behavesLikeSqlFormatter(format);
+  behavesLikePostgresqlFormatter(format);
   supportsComments(format, { nestedBlockComments: true });
   supportsCommentOn(format);
   supportsCreateView(format, { orReplace: true, ifNotExists: true });
@@ -130,33 +132,6 @@ describe('DuckDBFormatter', () => {
   supportsLimiting(format, { limit: true, offset: true });
   supportsDataTypeCase(format);
 
-  it('allows $ character as part of identifiers', () => {
-    expect(format('SELECT foo$, some$$ident')).toBe(dedent`
-      SELECT
-        foo$,
-        some$$ident
-    `);
-  });
-
-  it('formats type-cast operator without spaces', () => {
-    expect(format('SELECT 2 :: numeric AS foo;')).toBe(dedent`
-      SELECT
-        2::numeric AS foo;
-    `);
-  });
-
-  // issue #144 (unsolved)
-  // This is currently far from ideal.
-  it('formats SELECT DISTINCT ON () syntax', () => {
-    expect(format('SELECT DISTINCT ON (c1, c2) c1, c2 FROM tbl;')).toBe(dedent`
-      SELECT DISTINCT
-        ON (c1, c2) c1,
-        c2
-      FROM
-        tbl;
-    `);
-  });
-
   it('formats TIMESTAMP WITH TIME ZONE syntax', () => {
     expect(
       format(`
@@ -167,112 +142,6 @@ describe('DuckDBFormatter', () => {
         id INT PRIMARY KEY NOT NULL,
         created_at TIMESTAMP WITH TIME ZONE
       );
-    `);
-  });
-
-  // Regression test for issue #624
-  it('supports array slice operator', () => {
-    expect(format('SELECT foo[:5], bar[1:], baz[1:5], zap[:];')).toBe(dedent`
-      SELECT
-        foo[:5],
-        bar[1:],
-        baz[1:5],
-        zap[:];
-    `);
-  });
-
-  it('formats ALTER TABLE ... ALTER COLUMN', () => {
-    expect(
-      format(
-        `ALTER TABLE t ALTER COLUMN foo SET DATA TYPE VARCHAR;
-         ALTER TABLE t ALTER COLUMN foo SET DEFAULT 5;
-         ALTER TABLE t ALTER COLUMN foo DROP DEFAULT;
-         ALTER TABLE t ALTER COLUMN foo SET NOT NULL;
-         ALTER TABLE t ALTER COLUMN foo DROP NOT NULL;`
-      )
-    ).toBe(dedent`
-      ALTER TABLE t
-      ALTER COLUMN foo
-      SET DATA TYPE VARCHAR;
-
-      ALTER TABLE t
-      ALTER COLUMN foo
-      SET DEFAULT 5;
-
-      ALTER TABLE t
-      ALTER COLUMN foo
-      DROP DEFAULT;
-
-      ALTER TABLE t
-      ALTER COLUMN foo
-      SET NOT NULL;
-
-      ALTER TABLE t
-      ALTER COLUMN foo
-      DROP NOT NULL;
-    `);
-  });
-
-  // Issue #685
-  it('allows TYPE to be used as an identifier', () => {
-    expect(format(`SELECT type, modified_at FROM items;`)).toBe(dedent`
-      SELECT
-        type,
-        modified_at
-      FROM
-        items;
-    `);
-  });
-
-  // Issue #156, #709
-  it('does not recognize common fields names as keywords', () => {
-    expect(
-      format(`SELECT id, type, name, location, label, password FROM release;`, {
-        keywordCase: 'upper',
-      })
-    ).toBe(dedent`
-      SELECT
-        id,
-        type,
-        name,
-        location,
-        label,
-        password
-      FROM
-        release;
-    `);
-  });
-
-  it('formats DEFAULT VALUES clause', () => {
-    expect(
-      format(`INSERT INTO items default values RETURNING id;`, {
-        keywordCase: 'upper',
-      })
-    ).toBe(dedent`
-      INSERT INTO
-        items
-      DEFAULT VALUES
-      RETURNING
-        id;
-    `);
-  });
-
-  // Issue #726
-  it('treats TEXT as data-type (not as plain keyword)', () => {
-    expect(
-      format(`CREATE TABLE foo (items text);`, {
-        dataTypeCase: 'upper',
-      })
-    ).toBe(dedent`
-      CREATE TABLE foo (items TEXT);
-    `);
-
-    expect(
-      format(`CREATE TABLE foo (text VARCHAR(100));`, {
-        keywordCase: 'upper',
-      })
-    ).toBe(dedent`
-      CREATE TABLE foo (text VARCHAR(100));
     `);
   });
 
--- test/postgresql.test.ts
+++ test/postgresql.test.ts
@@ -30,12 +30,14 @@ import supportsOnConflict from './features/onConflict.js';
 import supportsIsDistinctFrom from './features/isDistinctFrom.js';
 import supportsArrayLiterals from './features/arrayLiterals.js';
 import supportsDataTypeCase from './options/dataTypeCase.js';
+import behavesLikePostgresqlFormatter from './behavesLikePostgresqlFormatter.js';
 
 describe('PostgreSqlFormatter', () => {
   const language = 'postgresql';
   const format: FormatFn = (query, cfg = {}) => originalFormat(query, { ...cfg, language });
 
   behavesLikeSqlFormatter(format);
+  behavesLikePostgresqlFormatter(format);
   supportsComments(format, { nestedBlockComments: true });
   supportsCommentOn(format);
   supportsCreateView(format, { orReplace: true, materialized: true, ifNotExists: true });
@@ -164,33 +166,6 @@ describe('PostgreSqlFormatter', () => {
   supportsLimiting(format, { limit: true, offset: true, fetchFirst: true, fetchNext: true });
   supportsDataTypeCase(format);
 
-  it('allows $ character as part of identifiers', () => {
-    expect(format('SELECT foo$, some$$ident')).toBe(dedent`
-      SELECT
-        foo$,
-        some$$ident
-    `);
-  });
-
-  it('formats type-cast operator without spaces', () => {
-    expect(format('SELECT 2 :: numeric AS foo;')).toBe(dedent`
-      SELECT
-        2::numeric AS foo;
-    `);
-  });
-
-  // issue #144 (unsolved)
-  // This is currently far from ideal.
-  it('formats SELECT DISTINCT ON () syntax', () => {
-    expect(format('SELECT DISTINCT ON (c1, c2) c1, c2 FROM tbl;')).toBe(dedent`
-      SELECT DISTINCT
-        ON (c1, c2) c1,
-        c2
-      FROM
-        tbl;
-    `);
-  });
-
   // Regression test for issue #447
   it('formats empty SELECT', () => {
     expect(format('SELECT;')).toBe(dedent`
@@ -216,49 +191,6 @@ describe('PostgreSqlFormatter', () => {
     `);
   });
 
-  // Regression test for issue #624
-  it('supports array slice operator', () => {
-    expect(format('SELECT foo[:5], bar[1:], baz[1:5], zap[:];')).toBe(dedent`
-      SELECT
-        foo[:5],
-        bar[1:],
-        baz[1:5],
-        zap[:];
-    `);
-  });
-
-  it('formats ALTER TABLE ... ALTER COLUMN', () => {
-    expect(
-      format(
-        `ALTER TABLE t ALTER COLUMN foo SET DATA TYPE VARCHAR;
-         ALTER TABLE t ALTER COLUMN foo SET DEFAULT 5;
-         ALTER TABLE t ALTER COLUMN foo DROP DEFAULT;
-         ALTER TABLE t ALTER COLUMN foo SET NOT NULL;
-         ALTER TABLE t ALTER COLUMN foo DROP NOT NULL;`
-      )
-    ).toBe(dedent`
-      ALTER TABLE t
-      ALTER COLUMN foo
-      SET DATA TYPE VARCHAR;
-
-      ALTER TABLE t
-      ALTER COLUMN foo
-      SET DEFAULT 5;
-
-      ALTER TABLE t
-      ALTER COLUMN foo
-      DROP DEFAULT;
-
-      ALTER TABLE t
-      ALTER COLUMN foo
-      SET NOT NULL;
-
-      ALTER TABLE t
-      ALTER COLUMN foo
-      DROP NOT NULL;
-    `);
-  });
-
   it('formats FOR UPDATE clause', () => {
     expect(
       format(`
@@ -278,69 +210,6 @@ describe('PostgreSqlFormatter', () => {
         tbl
       FOR UPDATE OF
         tbl.salary;
-    `);
-  });
-
-  // Issue #685
-  it('allows TYPE to be used as an identifier', () => {
-    expect(format(`SELECT type, modified_at FROM items;`)).toBe(dedent`
-      SELECT
-        type,
-        modified_at
-      FROM
-        items;
-    `);
-  });
-
-  // Issue #156, #709
-  it('does not recognize common fields names as keywords', () => {
-    expect(
-      format(`SELECT id, type, name, location, label, password FROM release;`, {
-        keywordCase: 'upper',
-      })
-    ).toBe(dedent`
-      SELECT
-        id,
-        type,
-        name,
-        location,
-        label,
-        password
-      FROM
-        release;
-    `);
-  });
-
-  it('formats DEFAULT VALUES clause', () => {
-    expect(
-      format(`INSERT INTO items default values RETURNING id;`, {
-        keywordCase: 'upper',
-      })
-    ).toBe(dedent`
-      INSERT INTO
-        items
-      DEFAULT VALUES
-      RETURNING
-        id;
-    `);
-  });
-
-  // Issue #726
-  it('treats TEXT as data-type (not as plain keyword)', () => {
-    expect(
-      format(`CREATE TABLE foo (items text);`, {
-        dataTypeCase: 'upper',
-      })
-    ).toBe(dedent`
-      CREATE TABLE foo (items TEXT);
-    `);
-
-    expect(
-      format(`CREATE TABLE foo (text VARCHAR(100));`, {
-        keywordCase: 'upper',
-      })
-    ).toBe(dedent`
-      CREATE TABLE foo (text VARCHAR(100));
     `);
   });
 
